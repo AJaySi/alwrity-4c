a7015a6ed5dfb1f88c2898f2a6218e9fd25835a8
ajaysi <ajay.calsoft@gmail.com>
2024-05-06 12:07:29 +0530

    AI Copywriting 4c Formula

--- copywriting_4c.py
+++ copywriting_4c.py
@@ -1,16 +1,15 @@
-import time
+import time #Iwish
 import os
 import json
-import openai
+import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
+import google.generativeai as genai
 from tenacity import retry, stop_after_attempt, wait_random_exponential
 
 def main():
     set_page_config()
     custom_css()
     hide_elements()
-    sidebar()
     title_and_description()
     input_section()
 
@@ -18,7 +17,6 @@ def set_page_config():
     st.set_page_config(
         page_title="Alwrity",
         layout="wide",
-        page_icon="img/logo.png"
     )
 
 def custom_css():
@@ -57,38 +55,10 @@ def hide_elements():
     hide_streamlit_footer = '<style>#MainMenu {visibility: hidden;} footer {visibility: hidden;}</style>'
     st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
 
-def sidebar():
-    st.sidebar.title("Clear-Concise-Credible-Compelling")
-    st.sidebar.image("img/alwrity.jpeg", use_column_width=True)
-    st.sidebar.markdown("🧕 :red[Checkout Alwrity], complete **AI writer & Blogging solution**:[Alwrity](https://alwrity.netlify.app)")
-
 
 def title_and_description():
     st.title("✍️ Alwrity - AI Generator for CopyWriting 4C's Formula")
-    with st.expander("What is **Copywriting 4C's formula** & **How to Use**? 📝❗"):
-        st.markdown('''
-            ### What's 4C's copywriting Formula, How to use this AI generator 🗣️
-            ---
-            #### 4C's Copywriting Formula
 
-            4C's stands for Clear, Concise, Credible, and Compelling. It's a copywriting formula that emphasizes:
-
-            1. **Clear**: Presenting information in a straightforward and understandable manner.
-            2. **Concise**: Communicating the message succinctly without unnecessary details.
-            3. **Credible**: Providing trustworthy and reliable information supported by evidence.
-            4. **Compelling**: Creating content that captivates and persuades the audience to take action.
-
-            The 4C's formula ensures that copy is effective, engaging, and impactful.
-
-            #### 4C's Copywriting Formula: Simple Example
-
-            - **Clear**: Our product is designed to simplify your daily tasks.
-            - **Concise**: Save time and effort with our user-friendly solution.
-            - **Credible**: Backed by years of research and customer testimonials.
-            - **Compelling**: Transform your productivity and streamline your workflow today!
-
-            ---
-        ''')
 
 def input_section():
     with st.expander("**PRO-TIP** - Campaign's Key features and benefits to build **Interest & Desire**", expanded=True):
@@ -125,63 +95,68 @@ def generate_four_cs_copy(brand_name, description, ad_details):
         Back up your claims with evidence or testimonials.
         Make your content interesting and persuasive.
     """
-    return openai_chatgpt(prompt)
-
-
-def page_bottom():
-    """ """
-    data_oracle = import_json(r"lottie_files/brain_robot.json")
-    st_lottie(data_oracle, width=600, key="oracle")
-
-    st.markdown('''
-    Copywrite using 4C's formula - powered by AI (OpenAI, Gemini Pro).
-
-    Implemented by [Alwrity](https://alwrity.netlify.app).
-
-    Learn more about [Google's Stance on AI generated content](https://alwrity.netlify.app/post/googles-guidelines-on-using-ai-generated-content-everything-you-need-to-know).
-    ''')
-
-    st.markdown("""
-    ### Problem:
-    Are you struggling to create compelling marketing campaigns that grab your audience's attention and drive them to take action?
-
-    ### Agitate:
-    Imagine spending hours crafting a message, only to find it doesn't resonate with your audience or compel them to engage with your brand. Your campaigns may lack the attention-grabbing headlines, compelling details, and persuasive calls-to-action needed to stand out in today's crowded digital landscape.
-
-    ### Conviction:
-    Introducing Alwrity - Your AI Generator for Copywriting 4C's Formula.
-    """)
+    try:
+        response = generate_text_with_exception_handling(prompt)
+        return response
+    except Exception as err:
+        st.error(f"Exit: Failed to get response from LLM: {err}")
+        exit(1)
 
 
 
 @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
-def openai_chatgpt(prompt, model="gpt-3.5-turbo-0125", max_tokens=500, top_p=0.9, n=1):
+def generate_text_with_exception_handling(prompt):
+    """
+    Generates text using the Gemini model with exception handling.
+
+    Args:
+        api_key (str): Your Google Generative AI API key.
+        prompt (str): The prompt for text generation.
+
+    Returns:
+        str: The generated text.
+    """
+
     try:
-        client = openai.OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-        response = client.chat.completions.create(
-            model=model,
-            messages=[{"role": "user", "content": prompt}],
-            max_tokens=max_tokens,
-            n=n,
-            top_p=top_p
-        )
-        return response.choices[0].message.content
-    except openai.APIError as e:
-        st.error(f"OpenAI API Error: {e}")
-    except openai.APIConnectionError as e:
-        st.error(f"Failed to connect to OpenAI API: {e}")
-    except openai.RateLimitError as e:
-        st.error(f"Rate limit exceeded on OpenAI API request: {e}")
-    except Exception as err:
-        st.error(f"An error occurred: {err}")
+        genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
 
+        generation_config = {
+            "temperature": 1,
+            "top_p": 0.95,
+            "top_k": 0,
+            "max_output_tokens": 8192,
+        }
 
-# Function to import JSON data
-def import_json(path):
-    with open(path, "r", encoding="utf8", errors="ignore") as file:
-        url = json.load(file)
-        return url
+        safety_settings = [
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+        ]
 
+        model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+                                      generation_config=generation_config,
+                                      safety_settings=safety_settings)
+
+        convo = model.start_chat(history=[])
+        convo.send_message(prompt)
+        return convo.last.text
+
+    except Exception as e:
+        st.exception(f"An unexpected error occurred: {e}")
+        return None
 
 
 if __name__ == "__main__":
